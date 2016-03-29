e9e11d7d1802b3178164f5d57feb5625add6827b
孙鹏 <287594495@qq.com>
2016-03-29 12:51:30 +0800

    注解开发配置完毕。

--- src/hit/controller/TestController.java
+++ src/hit/controller/TestController.java
@@ -4,12 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
-
+@Controller
 public class TestController extends AbstractController {
 
 	@Override
+	@RequestMapping(value="/hello.do")
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 			System.out.println("hello sunpeng");
